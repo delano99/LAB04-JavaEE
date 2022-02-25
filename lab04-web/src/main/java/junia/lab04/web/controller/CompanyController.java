eba620c2002ee908f96b14133aa4ddbafe612433
delano99 <delanonyotue@gmail.com>
2022-02-25 15:58:09 +0100

    create CompagnyController class and method

---
+++ lab04-web/src/main/java/junia/lab04/web/controller/CompanyController.java
@@ -0,0 +1,28 @@
+package junia.lab04.web.controller;
+
+import junia.lab04.core.entity.Company;
+import junia.lab04.core.service.CompanyService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+public class CompanyController {
+    private CompanyService companyService;
+
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String getListOfCompany(ModelMap modelMap) {
+        List<Company> companies = companyService.findAllWithProjects();
+
+        modelMap.put("companies", companies);
+
+        return "companyList";
+    }
+}
