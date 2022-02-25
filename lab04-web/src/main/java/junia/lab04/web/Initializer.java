2d0378a32b3fda2f90ebb131ed2075bb391a1d82
delano99 <delanonyotue@gmail.com>
2022-02-25 15:47:25 +0100

    create initializer class and method

---
+++ lab04-web/src/main/java/junia/lab04/web/Initializer.java
@@ -0,0 +1,26 @@
+package junia.lab04.web;
+
+import junia.lab04.core.config.AppConfig;
+import junia.lab04.core.config.DBConfig;
+import org.apache.cxf.transport.servlet.CXFServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+public class Initializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] { AppConfig.class, DBConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] { WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+            return new String[] { "/" };
+    }
+}
