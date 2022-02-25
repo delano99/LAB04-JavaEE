d667c6d70146a35f67468b44667e29aed5b8b051
delano99 <delanonyotue@gmail.com>
2022-02-25 17:54:10 +0100

    save new company

--- lab04-web/src/main/java/junia/lab04/web/controller/CompanyController.java
+++ lab04-web/src/main/java/junia/lab04/web/controller/CompanyController.java
@@ -1,12 +1,16 @@
 package junia.lab04.web.controller;
 
+import com.google.protobuf.compiler.PluginProtos;
 import junia.lab04.core.entity.Company;
 import junia.lab04.core.service.CompanyService;
+import org.hibernate.annotations.common.reflection.XMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.ServletRequest;
 import java.util.List;
 
 @Controller
@@ -24,5 +28,23 @@ public class CompanyController {
         modelMap.put("companies", companies);
 
         return "companiesList";
+    }
+
+    @RequestMapping(value = "/form", method = RequestMethod.GET)
+    public String getForm(ModelMap modelMap){
+
+        Company company = new Company();
+
+        modelMap.put("company", company);
+
+        return "companyForm";
+    }
+
+    @ModelAttribute("company")
+    @RequestMapping(value = "/form", method = RequestMethod.POST)
+    public String submitForm(Company company) {
+
+        companyService.save(company);
+        return "";
     }
 }
